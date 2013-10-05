ef060d4fd06e109cfae23b99bb663d63e70010c0
jake <bigjake2s@gmail.com>
2013-10-04 20:39:51 -0400

    Changed prefix spacing; using bold blue instead of normal blue for visibility

--- logger.go
+++ logger.go
@@ -7,9 +7,9 @@ import (
 const DefaultFlags = log.LstdFlags | log.Lshortfile | log.Lmicroseconds
 
 var (
-	Trace = log.New(NewColorWriter("b"), "TRACE ", DefaultFlags)
-	Debug = log.New(NewColorWriter("c"), "DEBUG ", DefaultFlags)
-	Info  = log.New(NewColorWriter("g"), "INFO  ", DefaultFlags)
-	Warn  = log.New(NewColorWriter("y"), "WARN  ", DefaultFlags)
-	Error = log.New(NewColorWriter("r"), "ERROR ", DefaultFlags)
+	Trace = log.New(NewColorWriter("!b"), "  TRACE ", DefaultFlags)
+	Debug = log.New(NewColorWriter("c"), "  DEBUG ", DefaultFlags)
+	Info  = log.New(NewColorWriter("g"), "   INFO ", DefaultFlags)
+	Warn  = log.New(NewColorWriter("y"), "   WARN ", DefaultFlags)
+	Error = log.New(NewColorWriter("r"), "  ERROR ", DefaultFlags)
 )
